4a049d297ed41678f6666e9ff91f049577ced3ad
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-04-28 18:53:39 -0500

    third commit

---
+++ src/app/crear-cancion/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React, { ReactNode, SetStateAction, useState } from "react";
+
+export default function page() {
+  const [namesong, setNameSong] = useState("");
+  const [autor, setAutor] = useState("");
+  const [url, setUrl] = useState("");
+
+  const handleChangeSong = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNameSong(e.target.value);
+  };
+
+  const handleChangeAutor = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAutor(e.target.value);
+  };
+
+  const handleChangeUrl = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    alert("Su cancion se guardo correctamente");
+  };
+  return (
+    <main className="flex justify-center items-center">
+      <section className="flex flex-col items-center border-2 w-full h-full">
+        <h1>Crear una cancion</h1>
+        <label>
+          Nombre Cancion:
+          <input
+            onChange={handleChangeSong}
+            value={namesong}
+            type="text"
+            placeholder="ingresa el nombre de una cancion"
+          />
+        </label>
+        <label>
+          Autor:
+          <input
+            value={autor}
+            onChange={handleChangeAutor}
+            type="text"
+            placeholder="ingresa el nombre del autor"
+          />
+        </label>
+        <label>
+          Url:
+          <input
+            value={url}
+            onChange={handleChangeUrl}
+            type="text"
+            placeholder="ingresa la url de la cancion"
+          />
+        </label>
+        <button onClick={handleSubmit}>Crear cancion</button>
+      </section>
+    </main>
+  );
+}
---
+++ src/app/playlists/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <h1 className="font-bold">Playlists</h1>
+    </div>
+  );
+}
